40d1b045ce28696d129c5605b526c584275f77b4
CarlosAlvarez96 <calvarezmartin96@gmail.com>
2023-05-10 13:52:58 +0200

    Primer y segundo ejercicio hechos

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const nameExists = persons.some(person => person.name === newName)
+
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(prevPersons => [...prevPersons, { name: newName }])
+      setNewName('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input type='text' value={newName} onChange={(event) => setNewName(event.target.value)} />
+          <div>number: <input /></div>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
